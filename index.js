b7b11103edde64a7a92dc2ba3688006b7ba90760
Hikaru Kurusu <92407425+HikaruKurusu@users.noreply.github.com>
2023-12-20 03:39:28 -0800

    Made red players and centered game

--- index.js
+++ index.js
@@ -5,3 +5,45 @@ canvas.width = 1024
 canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x,this.position.y,50,150)
+    }
+    update() {
+        this.draw()
+        this.position.y += 10
+    }
+}
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    velocity: {
+        x: 0,
+        y: 0,
+    }
+}) 
+const player2 = new Sprite({
+    position: {
+        x: 200,
+        y: 200,
+    },
+    velocity: {
+        x: 0,
+        y: 0,
+    }
+}) 
+player.draw()
+player2.draw()
+
+function animation() {
+    window.requestAnimationFrame(animation)
+}
+animation()
