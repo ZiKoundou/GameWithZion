db99659c2be32047d8833403db45860f7260049b
Hikaru Kurusu <92407425+HikaruKurusu@users.noreply.github.com>
2023-12-26 19:26:38 -0800

    movement added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ canvas.width = 1024
 canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
-
+const gravity = .2
 class Sprite {
     constructor({position, velocity}) {
         this.position = position
@@ -14,21 +14,24 @@ class Sprite {
         this.width = 50
     }
     draw() {
-        c.fillStyle = 'red'
+        c.fillStyle = 'blue'
         c.fillRect(this.position.x,this.position.y,this.width,this.height)
     }
     update() {
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if(this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y = 0
+        } else {
+            this.velocity.y += gravity
         }
     }
 }
 const player = new Sprite({
     position: {
-        x: 0,
+        x: 5,
         y: 0,
     },
     velocity: {
@@ -38,7 +41,7 @@ const player = new Sprite({
 }) 
 const player2 = new Sprite({
     position: {
-        x: 200,
+        x: 970,
         y: 200,
     },
     velocity: {
@@ -48,11 +51,99 @@ const player2 = new Sprite({
 }) 
 // player.draw()
 // player2.draw()
-
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    }
+}
 function animation() {
     window.requestAnimationFrame(animation)
-    c.clearRect(0,0,canvas.width,canvas.height)
+    c.fillStyle = 'black'
+    c.fillRect(0,0,canvas.width,canvas.height)
     player.update()
     player2.update()
+    if(keys.a.pressed == true) {
+        player.velocity.x = -1
+    } else if(keys.d.pressed == true) {
+        player.velocity.x = 1
+    } else {
+        player.velocity.x = 0
+    }
+
+    if(keys.ArrowLeft.pressed == true) {
+        player2.velocity.x = -1
+    } else if(keys.ArrowRight.pressed == true) {
+        player2.velocity.x = 1
+    } else {
+        player2.velocity.x = 0
+    }
 }
 animation()
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            // player.velocity.x = 1
+            keys.d.pressed = true
+            player.lastkey = 'd'
+            break
+        case 'a':
+            // player.velocity.x = -1
+            keys.a.pressed = true
+            player.lastkey = 'a'
+            break
+        case 'w':
+            player.velocity.y -= 10
+            break
+        case 'c':
+            player.attack()
+            break
+        case 'ArrowRight':
+            // player2.velocity.x = 1
+            keys.ArrowRight.pressed = true
+            player2.lastkey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            // player2.velocity.x = -1
+            keys.ArrowLeft.pressed = true
+            player2.lastkey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            player2.velocity.y -= 10
+            break
+        case '.':
+            // player2.isAttacking = true;
+            player2.attack()
+            break
+    }
+})
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            // player.velocity.x = 0
+            keys.d.pressed = false
+            break
+        case 'a':
+            // player.velocity.x = 0
+            keys.a.pressed = false
+            break
+        ///player 2 movements
+        case 'ArrowRight':
+            // player2.velocity.x = 0
+            keys.ArrowRight.pressed = false
+            break
+        case 'ArrowLeft':
+            // player2.velocity.x = 0
+            keys.ArrowLeft.pressed = false
+            break
+    }
+
+})
